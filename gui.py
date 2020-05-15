3bdf9fc80ab91cce043bf8e1b97dceb4668e9e2a
nathan <nathan.sardier@gmail.com>
2020-05-15 15:43:58 +0200

    tableau

--- gui.py
+++ gui.py
@@ -1,18 +1,23 @@
-
 import sys
 
 from PyQt5 import QtCore
 from PyQt5.QtWidgets import QMainWindow, QApplication, QAction, qApp, QWidget, QVBoxLayout, QTabWidget, QPushButton, \
-    QInputDialog, QLineEdit
+    QInputDialog, QLineEdit, QHBoxLayout, QHeaderView
+from PyQt5.QtWidgets import QTableWidget,QTableWidgetItem
+from PyQt5.uic.properties import QtGui
 
-
-
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
 
 class Gui(QMainWindow):
 
-    def __init__(self, parent=None):
+    def __init__(self,parent=None):
         super().__init__()
         self.initUI()
+
 
     def initUI(self):
         self.statusBar().showMessage('Ready')
@@ -20,20 +25,23 @@ class Gui(QMainWindow):
         menubar = self.menuBar()
         fichierMenu = menubar.addMenu("Fichier")
 
-        openAct = QAction("Ouvrir", self)
+        openAct = QAction("Ouvrir",self)
         openAct.triggered.connect(self.open)
         openAct.setShortcut('ctrl+O')
         openAct.setStatusTip('Ouvrir un fichier')
 
-        recAct = QAction("Enregistrer", self)
+        recAct = QAction("Enregistrer",self)
         recAct.triggered.connect(self.rec)
         recAct.setShortcut('ctrl+S')
         recAct.setStatusTip('Enregistrer un fichier')
 
-        quitAct = QAction("Quitter", self)
+
+        quitAct = QAction("Quitter",self)
         quitAct.triggered.connect(self.exit)
         quitAct.setShortcut('ctrl+Q')
         quitAct.setStatusTip('Quitter')
+
+
 
         fichierMenu.addAction(openAct)
         fichierMenu.addAction(recAct)
@@ -72,6 +80,7 @@ class MyTableWidget(QWidget):
         self.tab1 = QWidget()
         self.tab2 = QWidget()
 
+
         # Add tabs
         self.tabs.addTab(self.tab1, "Onglet 1")
         self.tabs.addTab(self.tab2, "Onglet 2")
@@ -82,8 +91,33 @@ class MyTableWidget(QWidget):
 
         self.tab1.layout.addWidget(openButton)
         self.tab1.setLayout(self.tab1.layout)
-        self.tab1.setStyleSheet(
-            _fromUtf8("background-image: url(./download.png); background-attachment: fixed"))
+        self.tab1.setStyleSheet(_fromUtf8("background-image: url(./download.png); background-attachment: fixed"))
+
+        # Left
+        self.table = QTableWidget()
+        self.table.setColumnCount(2)
+        self.table.setRowCount(6)
+        self.table.setItem(0, 0, QTableWidgetItem("Nom"))
+        self.table.setItem(1, 0, QTableWidgetItem("Prénom"))
+        self.table.setItem(2, 0, QTableWidgetItem("Date de naissance"))
+        self.table.setItem(3, 0, QTableWidgetItem("Sexe"))
+        self.table.setItem(4, 0, QTableWidgetItem("Taille"))
+        self.table.setItem(5, 0, QTableWidgetItem("Poids"))
+
+        # QWidget Layout
+        self.tab2.layout = QVBoxLayout()
+
+        #self.table_view.setSizePolicy(size)
+        self.tab2.layout.addWidget(self.table)
+
+        # Set the layout to the QWidget
+        self.tab2.setLayout(self.tab2.layout)
+
+        saveButton = QPushButton("Sauvegarder")
+        saveButton.clicked.connect(self.save)
+
+        self.tab2.layout.addWidget(saveButton)
+
 
         # Add tabs to widget
         self.layout.addWidget(self.tabs)
@@ -91,7 +125,8 @@ class MyTableWidget(QWidget):
 
     def openClick(self):
         print("click")
-        nom, type = QInputDialog.getText(self, "input dialog", "Votre Nom ?", QLineEdit.Normal, "")
+        nom,type = QInputDialog.getText(self,"input dialog","Votre Nom ?",QLineEdit.Normal,"")
         print(nom)
 
-
+    def save(self):
+        print("Save")
